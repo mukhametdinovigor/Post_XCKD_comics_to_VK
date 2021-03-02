cee9772c8c501dd04bd87a46e93f0fd2757c6e62
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-03-03 07:27:58 +1100

    Refactor code

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import random
 import dotenv
 import requests
 
+from urllib.parse import urlparse, unquote
+
 
 def get_random_comic_number(last_comic_url):
     response = requests.get(last_comic_url)
@@ -77,7 +79,7 @@ def get_vk_saving_uploading_photo_parameters(
     return photo_owner_id, photo_id
 
 
-def post_vk_wall(vk_url, method_name, owner_id, from_group, attachments, message, vk_access_token, vk_api_version):
+def post_photo_vk_wall(vk_url, method_name, owner_id, from_group, attachments, message, vk_access_token, vk_api_version):
     payload = {
         'owner_id': owner_id,
         'from_group': from_group,
@@ -91,20 +93,21 @@ def post_vk_wall(vk_url, method_name, owner_id, from_group, attachments, message
     return f'Your post_id - {response.json()["response"]["post_id"]}'
 
 
-def main():
-    dotenv.load_dotenv()
+def download_comic():
     last_comic_url = 'https://xkcd.com/info.0.json'
     random_comic_number = get_random_comic_number(last_comic_url)
     random_comic_url = f'https://xkcd.com/{random_comic_number}/info.0.json'
     comic_image_url, comic_title = get_comic_parameters(random_comic_url)
-    comic_file_name = comic_image_url.split('/')[-1]
+    comic_file_name = unquote(os.path.split(urlparse(comic_image_url).path)[1])
     download_image(comic_image_url, comic_file_name)
+    return comic_file_name, comic_title
+
+
+def upload_comic(comic_file_name, comic_title, vk_access_token, vk_group_id):
     vk_url = 'https://api.vk.com/method/'
     upload_method_name = 'photos.getWallUploadServer'
     save_method_name = 'photos.saveWallPhoto'
     wall_posting_method_name = 'wall.post'
-    vk_access_token = os.getenv('VK_ACCESS_TOKEN')
-    vk_group_id = os.getenv('VK_GROUP_ID')
     vk_api_version = '5.130'
     upload_url = get_vk_upload_url(vk_url, upload_method_name, vk_access_token, vk_api_version)
     server, photo, photo_hash = get_vk_uploading_photo_parameters(upload_url, comic_file_name)
@@ -112,9 +115,20 @@ def main():
         vk_url, save_method_name, server, photo, photo_hash, vk_access_token, vk_api_version)
     from_group = 1
     attachments = f'photo{photo_owner_id}_{photo_id}'
-    print(post_vk_wall(
-        vk_url, wall_posting_method_name, vk_group_id, from_group, attachments, comic_title, vk_access_token, vk_api_version))
-    os.remove(comic_file_name)
+    post_photo_vk_wall(
+        vk_url, wall_posting_method_name, vk_group_id, from_group, attachments, comic_title, vk_access_token,
+        vk_api_version)
+
+
+def main():
+    dotenv.load_dotenv()
+    try:
+        comic_file_name, comic_title = download_comic()
+        vk_access_token = os.getenv('VK_ACCESS_TOKEN')
+        vk_group_id = os.getenv('VK_GROUP_ID')
+        upload_comic(comic_file_name, comic_title, vk_access_token, vk_group_id)
+    finally:
+        os.remove(comic_file_name)
 
 
 if __name__ == '__main__':
